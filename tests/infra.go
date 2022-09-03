252f33d8c1611703e7af7f4165e175315218df2a
Leo <leonids@slim-it.lv>
2022-09-03 20:45:49 +0300

    wip

--- tests/infra.go
+++ tests/infra.go
@@ -61,7 +61,7 @@ func waitForPortListenStart(t *testing.T, port int) (conn net.Conn) {
 	var err error
 
 	addr := fmt.Sprintf("%s:%d", BindHost, port)
-	ctx, cancelFn := context.WithTimeout(context.Background(), 200*time.Millisecond)
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelFn()
 
 	poll := time.NewTicker(10 * time.Millisecond)
